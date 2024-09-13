ab10c9a17531aec08b7648e0b595f9857eb71344
Ryan <ryhime1@gmail.com>
2024-09-13 15:15:26 -0400

    Generates Assembly File

--- src/main/java/Cocktail/Main.java
+++ src/main/java/Cocktail/Main.java
@@ -6,6 +6,5 @@ package Cocktail;
 public class Main {
     public static void main(String[] args) {
         CodeGen codeGen = new CodeGen("HelloWorld.asm", "HelloWorld");
-        codeGen.AsmToObject();
     }
 }
